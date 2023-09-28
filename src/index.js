302dddcfcae025d9074980f1f77d1c4c026d2701
MantonySLX <Milton.Antony@SalesLabX.com>
2023-09-28 08:56:52 -0500

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your code here */}
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
